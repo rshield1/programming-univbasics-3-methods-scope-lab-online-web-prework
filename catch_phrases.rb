68c46e3e1d6d2a577f63e7363730e77de743049b
Robert Shields <robert.shieldsiv@gmail.com>
2019-11-25 08:56:09 +0000

    Automatically backed up by Learn

---
+++ catch_phrases.rb
@@ -0,0 +1,16 @@
+def mario
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts phrase
+end
+
+def toadstool
+  puts status
+end
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take This."
+end
+
+def any_phrase(phrase)
+  return phrase
+end
