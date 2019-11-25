af6c3927a89fbeefe77e18b381c2abca5034768d
Robert Shields <robert.shieldsiv@gmail.com>
2019-11-25 09:00:19 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,6 @@
 status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 def mario
-  status 
   puts phrase
 end
 
@@ -16,3 +15,5 @@ end
 def any_phrase(phrase)
   return phrase
 end
+
+any_phrase(phrase)
